de1b9be6c90c280e139df54df3df036e1eb49133
comemaino <mat.maino@gmail.com>
2022-06-01 18:09:02 +0200

    Add: AppFooter style

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,22 +2,30 @@
   <div>
     <section class="top-section">
       <div class="container">
-        <div class="row row-cols-4">
+        <div class="row row-cols-4 justify-content-between">
           <div class="col">
             <img src="../assets/img/logo.png" alt="" />
-            <p>A functional HTML Template for Corporate & Business.</p>
+            <p>A Functional HTML Template for Corporate & Business.</p>
             <div v-for="(contact, index) in contacts" :key="index">
-              <i :class="contact.iconClass"></i>
+              <i :class="contact.iconClass" class="mb-3 me-2"></i>
               {{ contact.info }}
             </div>
-            <button class="">get in touch</button>
+            <button class="btn alpha-btn">get in touch</button>
           </div>
-          <div v-for="(item, index) in footerLinks" :key="index" class="col">
-            <h4>{{ item.name }}</h4>
+          <div
+            v-for="(item, index) in footerLinks"
+            :key="index"
+            class="col links-col p-4"
+          >
+            <h4 class="mb-3">{{ item.name }}</h4>
             <ul>
-              <li v-for="(item, index) in item.contentList" :key="index">
-                <i class="fa-solid fa-chevron-right"></i>
-                <a href="item.href">{{ item.title }}</a>
+              <li
+                v-for="(item, index) in item.contentList"
+                :key="index"
+                class="mt-2"
+              >
+                <i class="fa-solid fa-chevron-right me-3"></i>
+                <a :href="item.href">{{ item.title }}</a>
               </li>
             </ul>
           </div>
@@ -25,17 +33,17 @@
       </div>
     </section>
 
-    <section class="bottom-section">
+    <div class="bottom-section py-4">
       <div class="container">
         <div class="row row-cols-2">
           <p>Enjoy the low price. We are tracking any intention of piracy</p>
 
           <p class="text-end">
-            &mark 2020 NEXGEN is Proudly Powered by <span>Codings</span>.
+            &copy; 2020 NEXGEN is Proudly Powered by <span>Codings</span>.
           </p>
         </div>
       </div>
-    </section>
+    </div>
   </div>
 </template>
 
@@ -52,4 +60,44 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "../style/variables.scss";
+* {
+  color: #fff;
+}
+.top-section {
+  background-color: $dark-section-bg-clr;
+
+  img {
+    height: 3rem;
+  }
+
+  .links-col {
+    width: 22%;
+    background-color: #1a1a1f;
+    border-radius: 0.5rem;
+
+    h4 {
+      text-transform: capitalize;
+    }
+
+    ul {
+      margin-left: -2rem;
+
+      li {
+        text-transform: capitalize;
+      }
+    }
+  }
+}
+
+.bottom-section {
+  background-color: #111;
+
+  p {
+    span {
+      color: $brand-primary-clr;
+    }
+  }
+}
+</style>
